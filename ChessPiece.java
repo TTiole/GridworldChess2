fd6750d59fd7c33fc14fd6a4d47ea62cb477b355
TTiole <eloi@gonzalezurbano.com>
2016-03-18 11:18:23 -0700

    Update ChessPiece.java

--- ChessPiece.java
+++ ChessPiece.java
@@ -1,4 +1,12 @@
 public abstract class ChessPiece extends Actor
 {
+	private boolean isWhite = false;
 	
+	public boolean isWhite()
+	{
+		return isWhite;
+	}
+	
+	public abstract void move();
+	public Location[] legalMoves();
 }
