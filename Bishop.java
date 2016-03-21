cebcd7eb63f08f2b5afe06893c8a2948d803206a
TTiole <eloi@gonzalezurbano.com>
2016-03-21 11:18:23 -0700

    Update Bishop.java

--- Bishop.java
+++ Bishop.java
@@ -1,4 +1,19 @@
+
+
 public class Bishop extends ChessPiece
 {
+  private location = super.getLocation();
   
+  public Bishop(boolean isWhite)
+  {
+    super(isWhite);
+  }
+  
+  public abstract Location[] getLegalMoves()
+  {
+    Location[13] legalMoves;
+    int count = 0;
+    int[4] directions = {NORTHEAST, SOUTHEAST, SOUTHWEST, NORTHWEST};
+    
+  }
 }
