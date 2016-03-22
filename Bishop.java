ad6d176e68a4ce264a1df011614a3a45e1841f1c
TTiole <eloi@gonzalezurbano.com>
2016-03-22 10:26:22 -0700

    Update Bishop.java

--- Bishop.java
+++ Bishop.java
@@ -9,9 +9,29 @@ public class Bishop extends ChessPiece
   
   public Location[] getLegalMoves()
   {
+    //cPos: current position being analyzed sPos: starting position nPos: next position to be analyzed
     Location[] legalMoves = new Location[13];
-    int count = 0;
-    int[] directions = {NORTHEAST, SOUTHEAST, SOUTHWEST, NORTHWEST};
+    Location sPos = getLocation();
+    for(int i = 1; i =< 4; i++)
+    {
+      switch(i) //NORTHEAST (1), SOUTHEAST (2), SOUTHWEST(3), NORTHWEST(4)
+      {
+        case 1:
+          if(getAdjacentLocation(NORTHEAST) == null)
+            break;
+          
+          break;
+        case 2:
+          //code
+          break;
+        case 3:
+          //code
+          break;
+        case 4:
+          //code
+          break;
+      }
+    }
     
   }
 }
