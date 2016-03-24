e9c89b0b597443cb30a508849c34e60d69c31752
PlatypusVenom <cjsmj00@gmail.com>
2016-03-24 07:08:35 -0700

    Update Rook.java

--- Rook.java
+++ Rook.java
@@ -7,6 +7,20 @@ public class Rook extends ChessPiece
 
     public Location[] getLegalMoves()
     {
+    List<Location> legalMoves = new ArrayList<Location>(0);
+    Location sPos = getLocation();
+    for(int i = 1; i <= 4; i++)
+    {
         
     }
+    
+    /*
+     * This converts ArrayLists into Arrays so that
+     * we can have arrays that will always be
+     * the right size.
+     */
+    Location[] legalMovesArray = new Location[legalMoves.size()];
+    legalMovesArray = legalMoves.toArray(legalMovesArray);
+    return legalMovesArray;
+  }
 }
