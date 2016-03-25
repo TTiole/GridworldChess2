64c41099b4bfadf09dc8e1be5f74f87424552a13
TTiole <eloi@gonzalezurbano.com>
2016-03-25 11:17:48 -0700

    Update King.java
    isInCheck method

--- King.java
+++ King.java
@@ -1,8 +1,31 @@
+import java.util.ArrayList;
+
 public class King extends ChessPiece
 {
+  private boolean check;
+  private ArrayList<Location> oppositeLocs;
+  
   public King(char c)
   {
+    char targetColor = 'w';
+    if(c == 'w')
+      targetColor = 'b';
+    oppositeLocs = getLocations(targetColor);
+  	check = false;
   	super(c);
+  }
+  
+  public boolean isInCheck()
+  {
+    for(Location L : oppositeLocs)
+    {
+      ChessPiece C = (ChessPiece)(getGrid().get(L));
+      Location[] enemyLegal = C.getLegalMoves();
+      for(Location L2 : enemyLegal)
+        if(enemyLegal[L2].equals(getLocation()))
+          return true;
+    }
+    return false;
   }
   
   public Location[] getLegalMoves()
