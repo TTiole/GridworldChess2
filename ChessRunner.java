c37054f433116ce68d45852465fa7de3610f0978
PlatypusVenom <cjsmj00@gmail.com>
2016-03-26 12:29:41 -0700

    Update ChessRunner.java

--- ChessRunner.java
+++ ChessRunner.java
@@ -20,11 +20,11 @@ public class ChessRunner
 			world.add(loc, history.get(k).get(loc));
 	}
 	
-	public static void recordState(Grid grid)
+	public static void recordState()
 	{
 		HashMap<Location, ChessPiece> places = new HashMap<Location, ChessPiece>(1);
-		for(Location loc : grid.getOccupiedLocations())
-			places.put(loc, (ChessPiece)(grid.get(loc))
+		for(Location loc : world.getGrid().getOccupiedLocations())
+			places.put(loc, (ChessPiece)(world.getGrid().get(loc))
 		history.add(0, places);
 	}
 	
@@ -33,25 +33,25 @@ public class ChessRunner
 		return history;
 	}
     
-    	public static void defaultSetup()
+    public static void defaultSetup()
+    {
+    	char[] colors = new char[2];
+    	colors[0] = 'b';
+    	colors[1] = 'w';
+    	
+    	for(int i = 0; i < colors.length; i++)
     	{
-    		char[] colors = new char[2];
-    		colors[0] = 'b';
-    		colors[1] = 'w';
-    		
-    		for(int i = 0; i < colors.length; i++)
-    		{
-    			for(int j = 0; j < 8; j++)
-    				world.add(new Location(6 - i*5, j), new Pawn(colors[i]));
-        		
-        		world.add(new Location(i*7, 0), new Rook(colors[i]));
-        		world.add(new Location(i*7, 1), new Knight(colors[i]));
-        		world.add(new Location(i*7, 2), new Bishop(colors[i]));
-        		world.add(new Location(i*7, 3), new Queen(colors[i]));
-        		world.add(new Location(i*7, 4), new King(colors[i]));
-        		world.add(new Location(i*7, 5), new Bishop(colors[i]));
-        		world.add(new Location(i*7, 6), new Knight(colors[i]));
-        		world.add(new Location(i*7, 7), new Rook(colors[i]));
-    		}
+    		for(int j = 0; j < 8; j++)
+    			world.add(new Location(6 - i*5, j), new Pawn(colors[i]));
+       		
+       		world.add(new Location(i*7, 0), new Rook(colors[i]));
+       		world.add(new Location(i*7, 1), new Knight(colors[i]));
+       		world.add(new Location(i*7, 2), new Bishop(colors[i]));
+       		world.add(new Location(i*7, 3), new Queen(colors[i]));
+       		world.add(new Location(i*7, 4), new King(colors[i]));
+       		world.add(new Location(i*7, 5), new Bishop(colors[i]));
+       		world.add(new Location(i*7, 6), new Knight(colors[i]));
+       		world.add(new Location(i*7, 7), new Rook(colors[i]));
     	}
+	}
 }
