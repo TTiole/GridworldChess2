bfce280f7cbbc341ae4881645748cf9650bb3a5a
PlatypusVenom <cjsmj00@gmail.com>
2016-03-25 19:10:29 -0700

    Update ChessRunner.java

--- ChessRunner.java
+++ ChessRunner.java
@@ -1,30 +1,24 @@
 public class ChessRunner
 {
-		private static ActorWorld world;
+	private static ActorWorld world;
 		
-    public static void main(String[] args)
-    {
-        world = new ActorWorld();
-        defaultSetup();
-        world.show();
-    }
+	public static void main(String[] args)
+	{
+        	world = new ActorWorld();
+        	defaultSetup();
+        	world.show();
+    	}
     
-    public static void defaultSetup()
-    {
+    	public static void defaultSetup()
+    	{
     		char[] colors = new char[2];
     		colors[0] = 'b';
     		colors[1] = 'w';
     		
     		for(int i = 0; i < colors.length; i++)
     		{
-    				world.add(new Location(6 - i*5, 0), new Pawn(colors[i]));
-        		world.add(new Location(6 - i*5, 1), new Pawn(colors[i]));
-        		world.add(new Location(6 - i*5, 2), new Pawn(colors[i]));
-        		world.add(new Location(6 - i*5, 3), new Pawn(colors[i]));
-        		world.add(new Location(6 - i*5, 4), new Pawn(colors[i]));
-        		world.add(new Location(6 - i*5, 5), new Pawn(colors[i]));
-        		world.add(new Location(6 - i*5, 6), new Pawn(colors[i]));
-        		world.add(new Location(6 - i*5, 7), new Pawn(colors[i]));
+    			for(int j = 0; j < 8; j++)
+    				world.add(new Location(6 - i*5, j), new Pawn(colors[i]));
         		
         		world.add(new Location(i*7, 0), new Rook(colors[i]));
         		world.add(new Location(i*7, 1), new Knight(colors[i]));
@@ -35,5 +29,5 @@ public class ChessRunner
         		world.add(new Location(i*7, 6), new Knight(colors[i]));
         		world.add(new Location(i*7, 7), new Rook(colors[i]));
     		}
-    }
+    	}
 }
