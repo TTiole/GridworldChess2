66f73359a3d09f592e921400390062f426d7b9f3
PlatypusVenom <cjsmj00@gmail.com>
2016-04-04 12:05:44 -0700

    Update Knight.java

--- Knight.java
+++ Knight.java
@@ -7,6 +7,45 @@ public class Knight extends ChessPiece
   
   public Location[] getLegalMoves()
   {
-  	return new Location[0];
+  	    //List<Location> legalMoves = new ArrayList<Location>(0);
+        Location[] legalMovesArray = new Location[14];
+        int i = 0;
+        
+        for(int dy = 1; dy <= 2; y++)
+        {
+            int dx = 3 - dy;
+            for(int n = 0; n < 4; n++)
+            {
+                if(n == 2)
+                    dy *= -1;
+                if(n % 2 == 0)
+                    dx *= -1; // FINISH THIS FIRST
+            }
+        }
+        
+        for(int d = 0; d < 360; d += 90)
+        {
+            Location loc = getLocation();
+            while(isLegal(loc) || loc.equals(getLocation()))
+            {
+                if(isLegal(loc))
+                {
+                    legalMovesArray[i] = loc;
+                    i++;
+                    if(getGrid().get(loc) != null)
+                				break;
+                }
+                loc = loc.getAdjacentLocation(d);
+            }
+        }
+    
+        /*
+         * This converts ArrayLists into Arrays so that
+         * we can have arrays that will always be
+         * the right size.
+         */
+        //Location[] legalMovesArray = new Location[legalMoves.size()];
+        //legalMovesArray = legalMoves.toArray(legalMovesArray);
+        return legalMovesArray;
   }
 }
