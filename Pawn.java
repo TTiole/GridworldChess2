f407dff21d944512f39d2aae17c136e98e386c86
TTiole <eloi@gonzalezurbano.com>
2016-04-07 08:40:20 -0700

    Update Pawn.java
    Finished for now

--- Pawn.java
+++ Pawn.java
@@ -21,7 +21,7 @@ public class Pawn extends ChessPiece
   
   public Location[] getLegalMoves()
   {
-    //nPosF = next position in front, n2PosF = 2 positions in front, passedEnP = passed enemy pawn (this if for killing passing pawns)
+    //nPosF = next position in front, n2PosF = 2 positions in front, passedEnP = passed enemy pawn (this is for killing passing pawns), diagP = piece in the diagonals
     Location nPosF = getLocation().getAdjacentLocation(0);
     Location n2PosF = nPosF.getAdjacentLocation(0);
     Location[] legalMoves = new Location[4];
@@ -29,26 +29,29 @@ public class Pawn extends ChessPiece
     if(canPass())
     {
       if(getGrid().get(nPosF) == null)
-        {
+      {
         legalMoves[0] = nPosF;
         if(getGrid().get(n2PosF) == null)
           legalMoves[1] = n2PosF;
-        }
+      }
     }
     else
       if(getGrid().get(nPosF) == null)
         legalMoves[0] = nPosF;
     for(int i = 0; i <= 1; i++)
     {
-      if(i == 0)
-      {
-        ChessPiece passedEnP = (ChessPiece)(getGrid().get(getLocation().getAdjacentLocation(90)));
-        if(passedEnP.getColorType() != getColorType() && passedEnP.isPassed())
-          legalMoves[1] = new Location(getLocation())
-      }
+      ChessPiece passedEnP = (ChessPiece)(getGrid().get(getLocation().getAdjacentLocation(90 + 180*i)));
+      if(passedEnP.getColorType() != getColorType() && passedEnP.isPassed())
+        legalMoves[1] = new Location(getLocation().getRow()+1 - 2*i, getLocation().getCol()-1);
+    }
+    for(int i = 0; i <= 1; i++)
+    {
+      ChessPiece diagP = (ChessPiece)(getGrid().get(getLocation().getAdjacentLocation(45 + 90*i)));
+      if(diagP.getColorType() != getColorType())
+        legalMoves[i+2] = diagP.getLocation();
     }
     
-  	return new Location[4];
+  	return legalMoves;
   }
   
   public void copyTo(Location loc)
