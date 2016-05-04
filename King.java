d3f363c234e37fe0185b660c70a42719e0dcb360
TTiole <eloi@gonzalezurbano.com>
2016-05-04 14:51:53 -0700

    Update King.java

--- King.java
+++ King.java
@@ -5,17 +5,20 @@ public class King extends ChessPiece
   private boolean check;
   private char targetColor;
   private ArrayList<Location> oppositeLocs = new ArrayList<Location>();
+  private int moves;
   
   public King(char c)
   {
   	super(c);
+  	moves = 0;
+  	cLaunched = false;
     targetColor = 'w';
     if(c == 'w')
       targetColor = 'b';
   	check = false;
   }
   
-  public boolean isInCheck()
+  public boolean isInCheck() //looks if it's in check for king in current location
   {
     oppositeLocs = getLocations(targetColor);
     System.out.println(oppositeLocs);
@@ -33,10 +36,53 @@ public class King extends ChessPiece
     return false;
   }
   
+  public boolean isInCheck(Location loc) //looks if it's in check if the king was in the passed location
+  {
+    oppositeLocs = getLocations(targetColor);
+    System.out.println(oppositeLocs);
+    for(Location L : oppositeLocs)
+    {
+      ChessPiece C = (ChessPiece)(getGrid().get(L));
+      if(!(C instanceof King))
+      {
+        Location[] enemyLegal = C.getLegalMoves();
+        for(Location L2 : enemyLegal)
+          if(L2 != null && L2.equals(loc))
+            return true;
+      }
+    }
+    return false;
+  }
+  
+  public void moveTo(Location loc)
+  {
+    moves++;
+    if(getLocation().getRow() - loc.getRow() > 1) // kingside
+    {
+      Location KRL = new Location(getLocation().getRow()-3,getLocation().getCol());
+      if(getGrid().get(KRL) != null)
+          Rook KR = (Rook)(getGrid().get(KRL));
+      //KR.cLaunched();
+    }
+    else if(loc.getRow() - getLocation().getRow() > 1) //queenside
+    {
+      Location QRL = new Location(getLocation().getRow()+4,getLocation().getCol());
+      if(getGrid().get(QRL) != null)
+        Rook QR = (Rook)(getGrid().get(QRL));
+      //QR.cLaunched(); I need to make that method on the rook class. cLaunched stands for Castling Launched
+    }
+    super.moveTo(loc);
+  }
+  
+  public boolean canCastle()
+  {
+    return moves == 0 && !isInCheck();
+  }
+  
   public Location[] getLegalMoves()
   {
         //List<Location> legalMoves = new ArrayList<Location>(0);
-        Location[] legalMovesArray = new Location[27];
+        Location[] legalMovesArray = new Location[10];
         int i = 0;
         for(int d = 0; d < 360; d += 45)
         {
@@ -48,7 +94,32 @@ public class King extends ChessPiece
                 i++;
             }
         }
-    
+        //Castling
+        Location QRL = new Location(getLocation().getRow()+4,getLocation().getCol());
+        Location KRL = new Location(getLocation().getRow()-3,getLocation().getCol());
+        //Gets rooks
+        if(getGrid().get(QRL) != null)
+          Rook QR = (Rook)(getGrid().get(QRL));
+        if(getGrid().get(KRL) != null)
+          Rook KR = (Rook)(getGrid().get(KRL));
+        
+        //Kingside Castling
+        if(KR.canCastle() && canCastle())
+        {
+          Location l1 = new Location(getLocation().getRow()-1,getLocation().getCol());
+          Location l2 = new Location(getLocation().getRow()-2,getLocation().getCol());
+          if((getGrid().get(l1) == null && getGrid().get(l2) == null) && (isInCheck(l1) && isInCheck(l2))) //First part makes sure that the locations are empty, second checks to see if check would be caused there
+            legalMovesArray[8] = l2;
+        }
+        
+        //Queenside Castling
+        if(QR.canCastle() && canCastle())
+        {
+          Location l3 = new Location(getLocation().getRow()+1,getLocation().getCol());
+          Location l4 = new Location(getLocation().getRow()+2,getLocation().getCol());
+          if((getGrid().get(l3) == null && getGrid().get(l4) == null) && (isInCheck(l3) && isInCheck(l4))) //First part makes sure that the locations are empty, second checks to see if check would be caused there
+            legalMovesArray[9] = l4;
+        }
         /*
          * This converts ArrayLists into Arrays so that
          * we can have arrays that will always be
