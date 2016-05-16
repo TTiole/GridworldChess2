e05c17bced372e5be28c4c55bdb89b87a1deca77
PlatypusVenom <cjsmj00@gmail.com>
2016-05-16 12:48:16 -0700

    Update Knight.java

--- Knight.java
+++ Knight.java
@@ -10,7 +10,7 @@ public class Knight extends ChessPiece
   	    //List<Location> legalMoves = new ArrayList<Location>(0);
         Location[] legalMovesArray = new Location[16];
         
-        if(!ChessBoard.isTurn(getColorType()))
+        if(!ChessBoard.isTurn(getColorType()) && check)
         	return legalMovesArray;
         	
         int i = 0;
