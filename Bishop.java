4c85622d38ff21ad7c17c81a26117c99ea871def
PlatypusVenom <cjsmj00@gmail.com>
2016-05-24 20:31:38 -0700

    Update Bishop.java

--- Bishop.java
+++ Bishop.java
@@ -15,28 +15,8 @@ public class Bishop extends ChessPiece
         if(!ChessBoard.isTurn(getColorType()) && check)
         	return legalMoves;
         
-        int i = 0;
-        for(int d = 45; d < 360; d += 90)
-        {
-            Location sPos = getLocation();
-            while((loc != null && loc.isOnBoard()) || sPos.equals(getLocation()))
-            {
-                if(loc != null && loc.isOnBoard())
-                {
-                    ChessPiece C = (ChessPiece)(getGrid().get(sPos));
-                    if(C != null)
-                    {
-                      if(isLegal(check, sPos))
-                        legalMoves[i] = sPos;
-                      break;
-                    }
-                    else if(isLegal(check, sPos))
-                      legalMoves[i] = sPos;
-                    i++;
-                }
-                sPos = sPos.getAdjacentLocation(d);
-            }
-        }
+        appendLinearMoves(legalMoves, 45, 90);
+        
         return legalMoves;
     }
   
